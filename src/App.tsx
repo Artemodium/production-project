ef97f8a0246d782aee0c85bee09fbf15c3fa7025
Artem <getcss@yandex.ru>
2023-05-04 21:34:39 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from "./components/Counter";
+import './index.sass'
+
+const App = () => {
+    return (
+        <div className='app'>
+            wdwqdwdwdgsbnsthsh
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
