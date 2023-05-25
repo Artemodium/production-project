33e9849020f362871a36b7f148a260ba4af17ee1
Artemodium <getcss@yandex.ru>
2023-05-25 16:11:01 +0500

    styles refactored

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
@@ -30,15 +30,22 @@ export const Sidebar = ({ className }: SidebarProps) => {
             }
         >
             <Button
+                className={cls.collapsedBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                square
+                size={ButtonSize.L}
                 data-testid="sidebar-toggle"
                 type="button"
                 onClick={onToggle}
             >
-                {t('Переключить')}
+                { collapsed ? '>' : '<' }
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher
+                    className={cls.lang}
+                    short={collapsed}
+                />
             </div>
         </div>
     );
