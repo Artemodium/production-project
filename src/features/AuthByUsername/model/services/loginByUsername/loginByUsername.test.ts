f6655be8b63f45f76295850218f5760ec41bb929
Artem <getcss@yandex.ru>
2023-06-19 21:20:13 +0500

    add tests for login module

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
-import { Dispatch } from '@reduxjs/toolkit'
-import { StateSchema } from 'app/providers/StoreProvider'
 import { userActions } from 'entities/User'
-import { TestAsyncThunk } from 'shared/lib/tests/testAsyncThunk/testAsyncThunk.test'
+import { TestAsyncThunk } from 'shared/lib/tests/testAsyncThunk/testAsyncThunk'
 import { loginByUsername } from './loginByUsername'
 
 jest.mock('axios')
