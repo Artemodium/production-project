0b24dae99f6ff74c6dbe2d98c18278190d795a65
Artem <getcss@yandex.ru>
2023-06-19 21:02:42 +0500

    add tests for login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginUserName/getLoginUserName.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getLoginUserName } from './getLoginUserName'
+
+describe('getLoginUserName.test', () => {
+    test('should return Username', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'Username',
+            },
+        }
+        expect(getLoginUserName(state as StateSchema)).toEqual('Username')
+    })
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = { }
+        expect(getLoginUserName(state as StateSchema)).toEqual('')
+    })
+})
---
+++ src/features/AuthByUsername/model/slice/LoginSlice.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit'
+import { LoginSchema } from 'features/AuthByUsername'
+import { loginActions, loginReducer } from 'features/AuthByUsername/model/slice/LoginSlice'
+
+describe('LoginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = {
+            username: '123',
+        }
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('123123'),
+        )).toStrictEqual({ username: '123123' })
+    })
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = {
+            password: '123',
+        }
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('123123'),
+        )).toStrictEqual({ password: '123123' })
+    })
+})
