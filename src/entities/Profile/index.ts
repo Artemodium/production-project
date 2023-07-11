ad7e59ef7d4918e560a8f1b9ab300b59714ce6cc
Artem <getcss@yandex.ru>
2023-07-11 07:13:37 +0500

    add updateProfileData

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,3 +1,8 @@
+import { useSelector } from 'react-redux'
+import { getProfileData } from 'entities/Profile/model/selectors/getProfileData/getProfileData'
+import { getProfileError } from 'entities/Profile/model/selectors/getProfileError/getProfileError'
+import { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileIsLoading/getProfileIsLoading'
+
 export {
     Profile,
     ProfileSchema,
@@ -13,5 +18,15 @@ export {
 } from './model/service/fetchProfileData/fetchProfileData'
 
 export {
+    updateProfileData,
+} from './model/service/updateProfileData/updateProfileData'
+
+export {
     ProfileCard,
 } from './ui/ProfileCard/ProfileCard'
+
+export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading'
+export { getProfileData } from './model/selectors/getProfileData/getProfileData'
+export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm'
+export { getProfileError } from './model/selectors/getProfileError/getProfileError'
+export { getProfileReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly'
--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,46 +1,87 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
-import { getProfileData } from 'entities/Profile/model/selectors/getProfileData/getProfileData'
-import { getProfileError } from 'entities/Profile/model/selectors/getProfileError/getProfileError'
-import { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileIsLoading/getProfileIsLoading'
-import { Text } from 'shared/ui/Text/Text'
-import { Button, ButtonTheme } from 'shared/ui/Button/Button'
+import { Text, TextAlign, TextTheme } from 'shared/ui/Text/Text'
 import { Input } from 'shared/ui/input/Input'
+import { Loader } from 'shared/ui/Loader/Loader'
 import cls from './ProfileCard.module.scss'
+import { Profile } from '../../model/type/profile'
 
 interface ProfileCardProps {
     className?: string
+    data?: Profile
+    error?: string
+    isLoading?: boolean
+    readonly?: boolean
+    onChangeLastname: (value?: string) => void
+    onChangeFirstname: (value?: string) => void
+    onChangeAge: (value?: string) => void
+    onChangeCity: (value?: string) => void
 }
 
-export const ProfileCard = ({ className }: ProfileCardProps) => {
+export const ProfileCard = (props: ProfileCardProps) => {
+    const {
+        className,
+        data,
+        error,
+        isLoading,
+        readonly,
+        onChangeFirstname,
+        onChangeLastname,
+        onChangeAge,
+        onChangeCity,
+    } = props
     const { t } = useTranslation('profile')
-    const data = useSelector(getProfileData)
-    const error = useSelector(getProfileError)
-    const isLoading = useSelector(getProfileIsLoading)
+
+    if (isLoading) {
+        return (
+            <div className={classNames(cls.ProfileCard, [className], { [cls.loading]: true })}>
+                <Loader />
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className={classNames(cls.ProfileCard, [className], { [cls.error]: true })}>
+                <Text
+                    theme={TextTheme.ERROR}
+                    title={t('Ошибка загрузки профиля')}
+                    text={t('Попробуйте обновить страницу')}
+                    align={TextAlign.CENTER}
+                />
+            </div>
+        )
+    }
 
     return (
-        <div className={classNames(cls.ProfileCard, [className], { })}>
-            <div className={cls.header}>
-                <div>
-                    <Text title={t('Профиль')} />
-                    <Button
-                        className={cls.editBtn}
-                        theme={ButtonTheme.OUTLINE}
-                    >
-                        {t('Редактировать')}
-                    </Button>
-                </div>
-            </div>
+        <div className={classNames(cls.ProfileCard, [className, error], { })}>
             <div className={cls.data}>
                 <Input
                     value={data?.first}
                     placeholder={t('Ваше имя')}
+                    onChange={onChangeFirstname}
+                    readonly={readonly}
                 />
                 <Input
                     value={data?.lastname}
                     placeholder={t('Ваша фамилия')}
                     className={cls.input}
+                    onChange={onChangeLastname}
+                    readonly={readonly}
+                />
+                <Input
+                    value={data?.age}
+                    placeholder={t('Ваш возраст')}
+                    className={cls.input}
+                    onChange={onChangeAge}
+                    readonly={readonly}
+                />
+                <Input
+                    value={data?.city}
+                    placeholder={t('Ваш город')}
+                    className={cls.input}
+                    onChange={onChangeCity}
+                    readonly={readonly}
                 />
             </div>
         </div>
