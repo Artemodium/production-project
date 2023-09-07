14154d39455a6d0e8bd7cb47985dc772e5faed1b
Artemodium <getcss@yandex.ru>
2023-09-07 08:34:45 +0500

    add useModal hook

---
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -0,0 +1,63 @@
+import {
+    MutableRefObject, useCallback, useEffect, useRef, useState,
+} from 'react'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { Mods } from '../../../lib/classNames/classNames'
+import cls from '../../../ui/Modal/Modal.module.scss'
+
+interface UseModalProps {
+    onClose?: () => void
+    isOpen?: boolean
+    lazy?: boolean
+    animationDelay: number
+}
+
+export function useModal(props: UseModalProps) {
+    const {
+        onClose, isOpen, animationDelay,
+    } = props
+    const [isClosing, setIsClosing] = useState(false)
+    const [isMounted, setIsMounted] = useState(false)
+    const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true)
+        }
+    }, [isOpen])
+
+    const close = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true)
+            timerRef.current = setTimeout(() => {
+                onClose()
+                setIsClosing(false)
+            }, animationDelay)
+        }
+    }, [animationDelay, onClose])
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            close()
+        }
+    }, [close])
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown)
+        }
+        return () => {
+            clearTimeout(timerRef.current)
+            window.removeEventListener('keydown', onKeyDown)
+        }
+    }, [isOpen, onKeyDown])
+
+    const mods: Mods = {
+        [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
+    }
+    return {
+        isClosing,
+        isMounted,
+        close,
+    }
+}
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,6 +1,7 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames'
 import React, { memo, ReactNode } from 'react'
 import { useTheme } from 'app/providers/ThemeProvider'
+import { useModal } from '../../lib/hooks/useModal/useModal'
 import { Overlay } from '../Overlay/Overlay'
 import cls from './Drawer.module.scss'
 import { Portal } from '../Portal/Portal'
@@ -10,6 +11,7 @@ interface DrawerProps {
     children: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
+    lazy?: boolean
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -18,7 +20,14 @@ export const Drawer = memo((props: DrawerProps) => {
         children,
         onClose,
         isOpen,
+        lazy,
     } = props
+
+    const { close, isClosing, isMounted } = useModal({
+        animationDelay: 300,
+        onClose,
+        isOpen,
+    })
     const { theme } = useTheme()
 
     const mods: Mods = {
@@ -28,7 +37,7 @@ export const Drawer = memo((props: DrawerProps) => {
     return (
         <Portal>
             <div className={classNames(cls.Drawer, [className, theme, 'app_drawer'], mods)}>
-                <Overlay onClick={onClose} />
+                <Overlay onClick={close} />
                 <div
                     className={cls.content}
                 >
